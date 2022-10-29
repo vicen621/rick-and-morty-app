5ee622a564cec78c412028c7ab800fe715f8478b
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-10-28 21:58:31 -0300

    modified the base url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rick-and-morty-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
